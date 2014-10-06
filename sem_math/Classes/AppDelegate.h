5aac0dd859e81854ae208c10e861fc39627e781f
SangChan Lee <sangchan82@gmail.com>
2014-10-06 14:08:38 +0900

    Initial Commit

---
+++ sem_math/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  sem_math
+//
+//  Created by SangChan on 2014. 10. 6..
+//  Copyright sangchan 2014년. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ sem_math/Classes/HelloWorldScene.h
@@ -0,0 +1,27 @@
+//
+//  HelloWorldScene.h
+//  sem_math
+//
+//  Created by SangChan on 2014. 10. 6..
+//  Copyright sangchan 2014년. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+// Importing cocos2d.h and cocos2d-ui.h, will import anything you need to start using Cocos2D v3
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+/**
+ *  The main scene
+ */
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
++ (HelloWorldScene *)scene;
+- (id)init;
+
+// -----------------------------------------------------------------------
+@end
